1db869871e6d1d90058cc381ffa05293e1d6da47
saheedibrahim <saolabram@gmail.com>
2022-06-01 07:32:34 +0100

    edited testing.py

--- main.py
+++ main.py
@@ -1,38 +1,70 @@
+from ast import alias
 import random
 
-mycomputerOption = ["Rock", "Paper", "Scissors"]
-yourOption = input("Pick between R for Rock, P for Paper or S for Scissors: " ).upper()
-while yourOption not in ['R', 'P', 'S']:
-    yourOption = input("Invalid input please pick between R, P or S: " ).upper()
+myList = ["Rock", "Paper", "Scissors"]
+Player = input("Pick between R for Rock, P for Paper or S for Scissors: " ).upper()
+while Player not in ['R', 'P', 'S']:
+    Player = input("Invalid input please pick between R, P or S: " ).upper()
 
 else:
-    computerOption = random.choice(mycomputerOption)
+    # R = myList[0]
+    # P = myList[1]
+    # S = myList[2]
+    alias 'R' is 'Rock'
+    'Paper' is 'P'
+    'Scisors' is 'R'
+    CPU = random.choice(myList)
+    # "R" as Rock
+    # if (Player == "R"):
+    #     Rock = Player
+    # elif (Player == "S"):
+    #     Scissors = Player
+    # else:
+    #     Paper = Player
+    if Player == CPU:
+        print(Player, CPU)
+        print("it is a tie")
 
-    if (yourOption == "R" and computerOption == "Rock"):
-        print("Computer option is: " + computerOption )
-        print("It is a Tie")
-    elif (yourOption == "R" and computerOption == "Scissors"):
-        print("Computer option is: " + computerOption ) 
-        print("You Win")
-    elif (yourOption == "R" and computerOption == "Paper"):
-        print("Computer option is: " + computerOption )   
-        print("The computer win")
-    elif (yourOption == "P" and computerOption == "Rock"):
-        print("Computer option is: " + computerOption )    
-        print("Win")
-    elif (yourOption == "P" and computerOption == "Scissors"):
-        print("Computer option is: " + computerOption )   
-        print("Lose")
-    elif (yourOption == "P" and computerOption == "Paper"):
-        print("Computer option is: " + computerOption )    
-        print("Tie")
-    elif (yourOption == "S" and computerOption == "Rock"):
-        print("Computer option is: " + computerOption )   
-        print("Lose")
-    elif (yourOption == "S" and computerOption == "Scissors"):
-        print("Computer option is: " + computerOption )    
-        print("Tie")
-    else:
-        (yourOption == "S" and computerOption == "Paper")
-        print("Computer option is: " + computerOption )  
-        print("Win")
+        # if ((Player == Rock and CPU == R) or (Player == Scissors and CPU == S) or (Player == Paper and CPU == P)):
+            # print("It is a tie")
+            # print(Player, CPU)
+    #     R = Player
+    # if Player == "P":
+    #     R = Player
+    # if Player == "R":
+    #     R = Player
+    # while (Player == CPU):
+    #     print("Tie")
+
+# print(R)
+
+    # while 
+    # (Player == "R" and CPU == "Rock"):
+    #     print("Player " + "(Rock)" + " : " + "CPU" + "(Rock)")
+    #     # print("Computer option is: " + CPU )
+    #     print("It is a Tie")
+    # elif (Player == "R" and CPU == "Scissors"):
+    #     print("Computer option is: " + CPU ) 
+    #     print("You Win")
+    # elif (Player == "R" and CPU == "Paper"):
+    #     print("Computer option is: " + CPU )   
+    #     print("The computer win")
+    # elif (Player == "P" and CPU == "Rock"):
+    #     print("Computer option is: " + CPU )    
+    #     print("You Win")
+    # elif (Player == "P" and CPU == "Scissors"):
+    #     print("Computer option is: " + CPU )   
+    #     print("The computer win")
+    # elif (Player == "P" and CPU == "Paper"):
+    #     print("Computer option is: " + CPU )    
+    #     print("It is a Tie")
+    # elif (Player == "S" and CPU == "Rock"):
+    #     print("Computer option is: " + CPU )   
+    #     print("The computer win")
+    # elif (Player == "S" and CPU == "Scissors"):
+    #     print("Computer option is: " + CPU )    
+    #     print("It is a Tie")
+    # else:
+    #     (Player == "S" and CPU == "Paper")
+    #     print("Computer option is: " + CPU )  
+    #     print("Win")
---
+++ testing.py
@@ -0,0 +1,33 @@
+import random
+myList = ["Rock", "Paper", "Scissors"]
+Player = input("Pick between R for Rock, P for Paper or S for Scissors: " ).upper()
+while Player not in ['R', 'P', 'S']:
+    Player = input("Invalid input please pick between R, P or S: " ).upper()
+else:    
+    CPU = random.choice(myList)
+    if((Player == "R" and CPU == "Rock") or (Player == "S" and CPU == "Scissors") or (Player == "P" and CPU == "Paper")):
+        print("Player (" + Player + ")" + " : " + "CPU (" + CPU + ")")
+        Player = input("It is a Tie pls replay: " ).upper()
+        # continue
+    else:
+        if (Player == "R" and CPU == "Scissors"):
+            print("Player (Rock)" + " : " +  "CPU (" + CPU + ")")
+            print("You Win")
+        elif (Player == "R" and CPU == "Paper"):
+            print("Player (Rock)" + " : " +  "CPU (" + CPU + ")")   
+            print("The computer win")
+        if (Player == "P" and CPU == "Scissors"):
+            print("Player (Paper)" + " : " +  "CPU (" + CPU + ")")
+            print("The computer win")
+        elif (Player == "P" and CPU == "Rock"):
+            print("Player (Paper)" + " : " +  "CPU (" + CPU + ")")   
+            print("You win")
+        if (Player == "S" and CPU == "Rock"):
+            print("Player (Scissors)" + " : " +  "CPU (" + CPU + ")")
+            print("The computer win")
+        elif (Player == "S" and CPU == "Paper"):
+            print("Player (Scissors)" + " : " +  "CPU (" + CPU + ")")   
+            print("You Win")
+        # else:
+        #     print("Player (" + Player + ")" + " : " +  "CPU (" + CPU + ")")
+        #     print("Error")
