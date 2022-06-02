5b67b5ebfc33da1a9911e5702d869c8bcb3f5a31
saheedibrahim <saolabram@gmail.com>
2022-06-02 23:18:12 +0100

    edit code

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import random
 myList = ["Rock", "Paper", "Scissors"]
-Player = input("Pick between R for Rock, P for Paper or S for Scissors: " ).upper()
+Player = input("Pick an option, R for Rock, P for Paper or S for Scissors: " ).upper()
 while Player not in ['R', 'P', 'S']:
-    Player = input("Invalid input please pick between R, P or S: " ).upper()
+    print("Invalid input" )
+    Player = input("replay: ").upper()
 else:    
     CPU = random.choice(myList)
-    while ((Player == "R" and CPU == "Rock") or (Player == "S" and CPU == "Scissors") or (Player == "P" and CPU == "Paper")):
-            print("Player (" + Player + ")" + " : " + "CPU (" + CPU + ")")
-            Player = input("It is a Tie pls replay: " ).upper()
+    user_option = ["R", "P", "S"]
+    user_index = user_option.index(Player)
+
+    user_option[0] = myList[0]
+    user_option[1] = myList[1]
+    user_option[2] = myList[2]
+    user = user_option[user_index]
+    while (user == CPU):
+        print("Player (" + user + ")" + " : " + "CPU (" + CPU + ")")
+        print("It is a Tie pls replay: " )
+        Player = input("replay: ").upper()
     else:
-        if (Player == "R" and CPU == "Scissors"):
-            print("Player (Rock)" + " : " +  "CPU (" + CPU + ")")
+        if (user == "Rock" and CPU == "Scissors"):
+            print("Player (" + user + ")" + " : " + "CPU (" + CPU + ")")
             print("You Win")
-        elif (Player == "R" and CPU == "Paper"):
-            print("Player (Rock)" + " : " +  "CPU (" + CPU + ")")   
-            print("The computer win")
-        if (Player == "P" and CPU == "Scissors"):
-            print("Player (Paper)" + " : " +  "CPU (" + CPU + ")")
-            print("The computer win")
-        elif (Player == "P" and CPU == "Rock"):
-            print("Player (Paper)" + " : " +  "CPU (" + CPU + ")")   
-            print("You win")
-        if (Player == "S" and CPU == "Rock"):
-            print("Player (Scissors)" + " : " +  "CPU (" + CPU + ")")
-            print("The computer win")
-        elif (Player == "S" and CPU == "Paper"):
-            print("Player (Scissors)" + " : " +  "CPU (" + CPU + ")")   
-            print("You Win")
+        elif (user == "Rock" and CPU == "Paper"):
+                print("Player (" + user + ")" + " : " + "CPU (" + CPU + ")") 
+                print("The computer win")
+        elif (user == "Paper" and CPU == "Scissors"):
+                print("Player (" + user + ")" + " : " + "CPU (" + CPU + ")")
+                print("The computer win")
+        elif (user == "Paper" and CPU == "Rock"):
+                print("Player (" + user + ")" + " : " + "CPU (" + CPU + ")") 
+                print("You win")            
+        elif (user == "Scissors" and CPU == "Rock"):
+                print("Player (" + user + ")" + " : " + "CPU (" + CPU + ")")
+                print("The computer win")
+        elif (user == "Scissors" and CPU == "Paper"):
+                print("Player (" + user + ")" + " : " + "CPU (" + CPU + ")") 
+                print("You Win")
